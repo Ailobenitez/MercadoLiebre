0dd1fd311930f9ae292fe659c5e88e5c6f1016e7
Ailobenitez <110872183+Ailobenitez@users.noreply.github.com>
2022-09-07 11:38:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,36 +4,17 @@ const path = require("path");
 
 app.use(express.static("./public"));
 
-app.listen(3030, ()=> 
-    console.log("esta funcionando http://localhost:3030")
-);
+app.listen(3000, ()=> 
+    console.log("esta funcionando http://localhost:3000")
+);app.get("/", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "./index.html"))
+});
 
 app.get("/", (req, res)=> {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+    res.sendFile(path.resolve(__dirname, "./apariencia.css"))
 });
-
-app.get("/register", (req, res)=> {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+app.use(express.static(path.resolve(__dirname, "./views/apariencia.css")
+))
+app.get("/", (req, res)=> {
+  res.sendFile(path.resolve(__dirname, "./public"))
 });
-
-app.post("/register", (req, res)=> {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
-});
-
-app.get("/login", (req, res)=> {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"))
-});
-const path = require('path')
-
-module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true
-  }
-}
